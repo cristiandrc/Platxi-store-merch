2e1e95149966759baccd8a6b9e4c4a1b5ec25f04
cristian <crisdarroca@hotmail.com>
2021-07-15 20:10:34 -0500

    cambios accesibility

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Product = ({ product, handleAddToCart }) => {
   return (
     <div className="Products-item">
-      <img src={product.image} all={product.title}></img>
+      <img src={product.image} alt={product.title}></img>
       <div className="Product-item-info">
         <h2>
           {product.title}
